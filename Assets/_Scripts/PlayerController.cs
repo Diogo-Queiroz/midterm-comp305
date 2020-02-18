ef590f8602797c56b2badedcbfd520cea32e9b9f
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-02-18 17:36:23 -0500

    lvl, movement, animations, cameracinemachine with boundaries

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // "Public" variables
+    [SerializeField] private float speed = 10.0f;
+    [SerializeField] private float jumpForce = 500.0f;
+    [SerializeField] private Transform groundCheckPos;
+    [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private float groundCheckRadius = 0.15f;
+    public bool aboveCrate;
+
+    // Private variables
+    public bool isGrounded = false;
+    private Rigidbody2D rBody;
+    private Animator anim;
+    private bool isFacingRight = true;
+    public bool painting = false;
+    public bool canMove = true;
+    VirtualCameraController cameraController;
+    float horiz;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraController = FindObjectOfType<VirtualCameraController>();
+        rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        StartCoroutine(Wait());
+        //AudioManager.instance.Play("LevelMusic");
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.6f);
+        VirtualCameraController._instance.TransitionTo(VirtualCameraController._instance.levelCam, VirtualCameraController._instance.playerCam,4.5f);
+
+    }
+
+    private void Update() {
+        horiz = Input.GetAxis("Horizontal");
+
+    }
+
+    // Physics
+    void FixedUpdate()
+    {
+
+        isGrounded = (GroundCheck() && rBody.velocity.y < 0.1f) || aboveCrate;
+
+        if(canMove){
+
+            // Jumping code will go here!
+            if(isGrounded && Input.GetAxis("Jump") > 0)
+            {
+                //AudioManager.instance.Play("Jump");
+                rBody.AddForce(new Vector2(0.0f, jumpForce));
+                isGrounded = false;
+            }
+
+            rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
+
+            if ( Mathf.Abs(horiz) > 0.1f || !isGrounded)
+            {
+                //anim.SetBool("Painting", false);
+                painting = false;
+            }
+        } else
+        {
+            rBody.velocity = new Vector2(0, rBody.velocity.y);
+        }
+
+        // Check if sprite needs to be flipped
+        if(isFacingRight && rBody.velocity.x < 0)
+        {
+            Flip();
+        }
+        else if (!isFacingRight && rBody.velocity.x > 0)
+        {
+            Flip();
+        }
+
+
+        anim.SetFloat("xSpeed", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("ySpeed", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private bool GroundCheck()
+    {
+        return Physics2D.OverlapCircle(groundCheckPos.position, groundCheckRadius, whatIsGround); ;
+    }
+
+    private void Flip()
+    {
+        Vector3 temp = transform.localScale;
+        temp.x *= -1;
+        transform.localScale = temp;
+
+        isFacingRight = !isFacingRight;
+    }
+}
---
+++ Assets/_Scripts/VirtualCameraController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[ExecuteInEditMode]
+public class VirtualCameraController : MonoBehaviour
+{
+    public static VirtualCameraController _instance;
+    public List<GameObject> virtualCameras;
+    public GameObject playerCam;
+    public GameObject levelCam;
+    PlayerController player;
+    void Awake()
+    {
+        if(_instance == null)
+        {
+            _instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        player = FindObjectOfType<PlayerController>();
+
+        virtualCameras.Clear();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            virtualCameras.Add(transform.GetChild(i).gameObject);
+        }
+
+
+    }
+
+    public void TransitionTo(GameObject cameraToTransitionTo)
+    {
+        for (int i = 0; i < virtualCameras.Count; i++)
+        {
+            if (virtualCameras[i] == cameraToTransitionTo)
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 10;
+            }
+            else
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 5;
+            }
+        }
+
+    }
+
+    public void TransitionTo(GameObject cameraToTransitionTo, GameObject cameraOut, float timeToWait)
+    {
+        for (int i = 0; i < virtualCameras.Count; i++)
+        {
+            if (virtualCameras[i] == cameraToTransitionTo)
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 10;
+            }
+            else
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 5;
+            }
+        }
+        StartCoroutine(TransitionBack(cameraOut,timeToWait));
+    }
+
+    public IEnumerator TransitionBack(GameObject cameraOut, float timeToWait)
+    {
+        yield return new WaitForSeconds(timeToWait);
+        player.canMove = true;
+        for (int i = 0; i < virtualCameras.Count; i++)
+        {
+            if (virtualCameras[i] == cameraOut)
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 10;
+            }
+            else
+            {
+                virtualCameras[i].GetComponent<CinemachineVirtualCamera>().Priority = 5;
+            }
+        }
+    }
+}
