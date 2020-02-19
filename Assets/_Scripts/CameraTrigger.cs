14633cff3308b79d29536b42bc1b983d3c326df7
Diogo Queiroz <diogoqueirtooz1212@gmail.com>
2020-02-19 11:31:32 -0500

    new cinemachine camera for treasure

---
+++ Assets/_Scripts/CameraTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+    private VirtualCameraController cameraController;
+
+    [SerializeField] private GameObject cameraToTransition;
+    [SerializeField] private GameObject cameraPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraController = FindObjectOfType<VirtualCameraController>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            VirtualCameraController._instance.TransitionTo(cameraToTransition);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            VirtualCameraController._instance.TransitionTo(cameraPlayer);
+        }
+    }
+}
